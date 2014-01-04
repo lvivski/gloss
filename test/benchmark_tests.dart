56acc5482064ed13dad35afaa64e90351528d20a
martyglaubitz <martyglaubitz@gmail.com>
2014-01-04 19:33:11 +0100

    add: benchmarks of transform operations in tests

---
+++ test/benchmark_tests.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:gloss/gloss.dart';
+import 'package:gloss/src/lexer.dart';
+import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
+
+void main() {
+  final stopWatch = new Stopwatch();
+  
+  runBenchmarks (FileSystemEntity fileEntity) {
+    if (!fileEntity.path.endsWith('yaml')) {
+      return;
+    }
+    
+    var yamlContent = new File(fileEntity.path).readAsStringSync();
+    final Map yamlDoc = loadYaml(yamlContent);
+    
+    for (var groupName in yamlDoc.keys) {
+      final Map cases = yamlDoc[groupName];
+        
+      for (var testCaseName in cases.keys) {
+        final List testCase = cases[testCaseName];
+
+        for (var i = 0, len = testCase.length; i < len; i++) {
+          final spec = testCase[0];
+
+          stopWatch.start();
+          
+          switch (spec['transformer']) {
+            case 'parser':
+              Gloss.parse(spec['code']);
+                  
+              break;
+                  
+            case 'lexer':
+              new Lexer(spec['code']).tokenize();
+                  
+              break;
+            default:
+              break;
+          }
+          
+          
+          stopWatch.stop();
+          print("TRANSFORM: ${groupName}/${testCaseName} #${i+1}: took ${stopWatch.elapsedMilliseconds} ms");
+          stopWatch.reset();
+        }           
+      }
+    }
+  }
+  
+  final files = new Directory(path.dirname(Platform.script.path)).listSync();
+  files.forEach(runBenchmarks);
+}
