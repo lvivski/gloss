f74ed41048464d172c6a0c386ca47d8e08bc2e2b
martyglaubitz <martyglaubitz@gmail.com>
2014-01-05 22:17:05 +0100

    add: AST viewer tool

---
+++ tool/astviewer/astviewer.dart
@@ -0,0 +1,29 @@
+import 'dart:html';
+
+import 'packages/gloss/src/lexer.dart';
+
+main () {
+  final sourceTextArea = querySelector('textarea');
+  final astTable = querySelector('.floatbox.ast');
+  final validatorBuilder = new NodeValidatorBuilder();
+  validatorBuilder.allowHtml5();
+  
+  renderAST (source) {
+    new Lexer(source).tokenize().forEach((List token) {
+      var tokenName = token[0];
+      var tokenParams = token.sublist(1);
+      
+      astTable.append(new Element.html([
+        '<div class="token">',
+          '<span class="name">', tokenName,'</span>','<span class="args">', tokenParams.join(' '), '</span>',
+        '</div>'
+      ].join(), validator: validatorBuilder));
+    });
+  }
+  
+  HttpRequest.getString('astviewer.gloss').then((glossStyles) {
+    sourceTextArea.value = glossStyles;
+  });
+  
+  querySelector('.button-lexer').onClick.listen((MouseEvent event) => renderAST(sourceTextArea.value));
+}
