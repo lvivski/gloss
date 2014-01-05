f1f3d8fdf286b595d7e2121ebb229586ba2fd240
martyglaubitz <martyglaubitz@gmail.com>
2014-01-05 23:09:34 +0100

    change: add option to perform rewrite on AST

--- tool/astviewer/astviewer.dart
+++ tool/astviewer/astviewer.dart
@@ -1,15 +1,25 @@
 import 'dart:html';
 
+import 'packages/gloss/src/rewriter.dart';
 import 'packages/gloss/src/lexer.dart';
 
 main () {
   final sourceTextArea = querySelector('textarea');
+  final performRewrite = querySelector('#performRewrite');
   final astTable = querySelector('.floatbox.ast');
   final validatorBuilder = new NodeValidatorBuilder();
   validatorBuilder.allowHtml5();
   
   renderAST (source) {
-    new Lexer(source).tokenize().forEach((List token) {
+    var tokens = new Lexer(source).tokenize();
+    
+    if (performRewrite.checked) {
+      tokens = new Rewriter(tokens).rewrite();
+    }
+    
+    astTable.children.clear();
+    
+    tokens.forEach((List token) {
       var tokenName = token[0];
       var tokenParams = token.sublist(1);
       
