ab2f070f39c68956cbc80dd24f98587b26ea6ae6
Yehor Lvivski <lvivski@gmail.com>
2014-01-13 15:28:57 +0200

    typo and packages fix

--- tool/astviewer/astviewer.dart
+++ tool/astviewer/astviewer.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
 
-import 'packages/gloss/src/rewriter.dart';
-import 'packages/gloss/src/lexer.dart';
+import 'package:gloss/src/rewriter.dart';
+import 'package:gloss/src/lexer.dart';
 
 main () {
   final sourceTextArea = querySelector('textarea');
@@ -9,20 +9,20 @@ main () {
   final astTable = querySelector('.floatbox.ast');
   final validatorBuilder = new NodeValidatorBuilder();
   validatorBuilder.allowHtml5();
-  
+
   renderAST (source) {
     var tokens = new Lexer(source).tokenize();
-    
+
     if (performRewrite.checked) {
       tokens = new Rewriter(tokens).rewrite();
     }
-    
+
     astTable.children.clear();
-    
+
     tokens.forEach((List token) {
       var tokenName = token[0];
       var tokenParams = token.sublist(1);
-      
+
       astTable.append(new Element.html([
         '<div class="token">',
           '<span class="name">', tokenName,'</span>','<span class="args">', tokenParams.join(' '), '</span>',
@@ -30,10 +30,10 @@ main () {
       ].join(), validator: validatorBuilder));
     });
   }
-  
+
   HttpRequest.getString('astviewer.gloss').then((glossStyles) {
     sourceTextArea.value = glossStyles;
   });
-  
+
   querySelector('.button-lexer').onClick.listen((MouseEvent event) => renderAST(sourceTextArea.value));
 }
